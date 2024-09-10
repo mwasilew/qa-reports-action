6274441a48172ce25b5ba609597e476e6a7fda23
Milosz Wasilewski <milosz.wasilewski@gmail.com>
2024-09-10 12:07:09 +0100

    Fix data submission
    Signed-off-by: Milosz Wasilewski <milosz.wasilewski@gmail.com>

--- submit.js
+++ submit.js
@@ -9,6 +9,7 @@ const qa_reports_project = core.getInput('qa_reports_project', { required: true 
 const qa_reports_environment = core.getInput('qa_reports_environment', { required: true })
 const qa_reports_build = core.getInput('qa_reports_build', { required: true })
 const qa_reports_patch_source = core.getInput('qa_reports_patch_source')
+const qa_reports_lava_backend = core.getInput('qa_reports_lava_backend', { required: true })
 // read from file
 const fs = require ('fs');
 var file;
@@ -21,12 +22,12 @@ try {
 // submit to lava
 const https = require('https');
 
-var postData = JSON.stringify({
-  'definition' : file
-});
-
 if ( github.event_name == 'pull_request' && qa_reports_patch_source ) {
 	// create new build with patch source first
+    var buildData = JSON.stringify({
+		  'patch_id': patch_id,
+		  'patch_source': qa_reports_patch_source
+    });
 
 	// patch-id = owner/repo/ref
 	const patch_id = github.repository + '/' + github.sha
@@ -37,14 +38,9 @@ if ( github.event_name == 'pull_request' && qa_reports_patch_source ) {
 	  method: 'POST',
 	  headers: {
 		   'Content-Type': 'application/json',
-		   'Content-Length': postData.length,
+		   'Content-Length': buildData.length,
 		   'Auth-Token': qa_reports_token
-		 },
-	  data: {
-		  'patch_id': patch_id,
-		  'patch_source': qa_reports_patch_source
-	     }
-
+		 }
 	};
 	var req1 = https.request(options, (res) => {
 	  console.log('statusCode:', res.statusCode);
@@ -60,9 +56,14 @@ if ( github.event_name == 'pull_request' && qa_reports_patch_source ) {
 	  core.setFailed(e.message);
 	});
 
-	req1.write(postData);
+	req1.write(buildData);
 	req1.end();
 }
+
+var postData = JSON.stringify({
+  'definition' : file,
+  'backend' : qa_reports_lava_backend
+});
 
 var options = {
   hostname: qa_reports_url,
@@ -73,7 +74,12 @@ var options = {
        'Content-Type': 'application/json',
        'Content-Length': postData.length,
        'Auth-Token': qa_reports_token
+     },
+  data: {
+	  'definition': ,
+	  'backend': ''
      }
+
 };
 
 var req2 = https.request(options, (res) => {
